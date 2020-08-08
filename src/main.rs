8364135a2885ec31af4bf1fe171f9f4c8e942f12
llxzy <snowcrash@tuta.io>
2020-08-08 12:07:18 +0200

    Changed structs a bit

--- src/main.rs
+++ src/main.rs
@@ -14,34 +14,36 @@ enum Action {
 }
 
 struct Game {
-    game_state: GameState
+    game_state: GameState,
+    max_turns: u32
 }
 
 struct GameState {
     current_turn: u32,
-    max_turns: u32
+    //max_turns: u32
+    ship: ship::Ship
 }
 
 impl Game {
     fn new(max_turns: u32) -> Game {
         Game {
-            game_state: GameState::new(max_turns)
+            game_state: GameState::new(max_turns),
+            max_turns
         }        
     }
 
     fn game_loop(&mut self) {
-        let mut ship = ship::Ship::new(self.game_state.max_turns);
         let mut distance = 100;
-        while self.game_state.current_turn < self.game_state.max_turns {
+        while self.game_state.current_turn < self.max_turns {
             println!("");
-            ship.display();
-            println!("Turns remaining: {}", self.game_state.max_turns - self.game_state.current_turn);
+            self.game_state.ship.display();
+            println!("Turns remaining: {}", self.max_turns - self.game_state.current_turn);
             let action: Action = user_choice();
             match action {
-                Action::Scavenge => ship.scavenge(),
-                Action::Refuel => ship.refuel(),
+                Action::Scavenge => self.game_state.ship.scavenge(),
+                Action::Refuel => self.game_state.ship.refuel(),
                 Action::Jump => {
-                    let jmp = ship.jump();
+                    let jmp: bool = self.game_state.ship.jump();
                     if jmp {
                         distance -= 10;
                         println!("You jump through hyperspace.");
@@ -54,7 +56,7 @@ impl Game {
                 }
             }
             if distance == 0 {
-                println!("Congratulations, you win on turn {}", self.game_state.current_turn);
+                println!("Congratulations, you won on turn {}", self.game_state.current_turn);
                 return;
             }
             self.game_state.advance_turn();            
@@ -67,7 +69,8 @@ impl GameState {
     fn new(max_turns: u32) -> GameState {
         GameState {
             current_turn: 0,
-            max_turns
+            ship: ship::Ship::new(max_turns)
+
         }
     }
 
