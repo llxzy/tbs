aef618f2bda2c626a95a9f6b6090e35382eb8f02
llxzy <snowcrash@tuta.io>
2020-08-11 18:16:29 +0200

    Changed main.rs to use utils.rs, cleaned up parsing

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,15 @@ use std::env;
 
 mod game;
 mod ship;
+mod utils;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() != 3 {
         panic!("Usage: cargo run <rounds> <distance>");
     }
-    let rounds: u32 = match args[1].parse::<u32>() {
-        Ok(num) => num,
-        Err(_) => panic!("<rounds> needs to be a number."),
-    };
-    let distance: i32 = match args[2].parse::<i32>() {
-        Ok(num) => num,
-        Err(_) => panic!("<distance> needs to be a number."),
-    };
+    let rounds: u32 = utils::parse(&args[1]);
+    let distance: i32 = utils::parse(&args[2]);
     let mut game = game::Game::new(rounds, distance); //TODO prettier declaration?
     game.game_loop();
 }
