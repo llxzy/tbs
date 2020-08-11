d1771a27d035aa88636f36094d2552a93b3513b9
llxzy <snowcrash@tuta.io>
2020-08-11 15:17:25 +0200

    Added typing to main() variables

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,18 @@ mod game;
 mod ship;
 
 fn main() {
-    //TODO check for best practices, do better error checking etc
     let args: Vec<_> = env::args().collect();
     if args.len() != 3 {
         panic!("Usage: cargo run <rounds> <distance>");
     }
-    let rounds = args[1].parse::<u32>().unwrap();
-    let distance = args[2].parse::<i32>().unwrap();
+    let rounds: u32 = match args[1].parse::<u32>() {
+        Ok(num) => num,
+        Err(_) => panic!("<rounds> needs to be a number.")
+    };
+    let distance: i32 = match args[2].parse::<i32>() {
+        Ok(num) => num,
+        Err(_) => panic!("<distance> needs to be a number.")
+    };
     let mut game = game::Game::new(rounds, distance); //TODO prettier declaration?
     game.game_loop();
 }
