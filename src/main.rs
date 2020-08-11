96a5332931b931e75a677fbe9243ac0157c0b946
llxzy <snowcrash@tuta.io>
2020-08-11 21:37:55 +0200

    Added random values for <rounds> and <distance> if not given as input

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,31 @@ mod game;
 mod ship;
 mod utils;
 
+// TODO high score system
+
 fn main() {
     let args: Vec<_> = env::args().collect();
-    if args.len() != 3 {
+    if args.len() == 3 {
         panic!("Usage: cargo run <rounds> <distance>");
     }
-    let rounds: u32 = utils::parse(&args[1]);
-    let distance: i32 = utils::parse(&args[2]);
-    let mut game = game::Game::new(rounds, distance); //TODO prettier declaration?
+    let rounds: u32;
+    let mut distance: i32;
+    match args.len() {
+        3 => {
+            rounds = utils::parse(&args[1]);
+            distance = utils::parse(&args[2])
+        }
+        1 => {
+            rounds = (rand::random::<u32>() % 20) + 10;
+            distance = rand::random::<i32>() % 100;
+            if distance < 0 {
+                distance *= -1;
+            }
+        }
+        _ => {
+            panic!("Usage: cargo run [rounds] [distance]");
+        }
+    }
+    let mut game = game::Game::new(rounds, distance);
     game.game_loop();
 }
