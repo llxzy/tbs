4861eecdd17bd6d5ca36cdbf2efdfe5ea58809f7
llxzy <snowcrash@tuta.io>
2020-08-11 21:41:38 +0200

    removed redundant code

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,15 @@ mod utils;
 fn main() {
     let args: Vec<_> = env::args().collect();
     let rounds: u32;
-    let mut distance: i32;
+    let distance: i32;
     match args.len() {
         3 => {
             rounds = utils::parse(&args[1]);
             distance = utils::parse(&args[2])
         }
         1 => {
-            rounds = (rand::random::<u32>() % 20) + 10;
-            distance = rand::random::<i32>() % 100;
-            if distance < 0 {
-                distance *= -1;
-            }
+            rounds = (rand::random::<u32>() % 15) + 10;
+            distance = (rand::random::<u32>() % 100) as i32;
         }
         _ => {
             panic!("Usage: cargo run [rounds] [distance]");
